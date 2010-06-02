6e98abbae79464c3a271d49ff756f6dfaf353a6e
Matias Särs <msevens@kolumbus.fi>
2010-06-02 21:27:44 +0300

    Version x.0.39.1 - README, CHANGELOG and version number changed.

--- dockbarx/dockbar.py
+++ dockbarx/dockbar.py
@@ -36,7 +36,7 @@ from cairowidgets import *
 from theme import Theme, NoThemesError
 from common import *
 
-VERSION = 'x.0.39+bzr'
+VERSION = 'x.0.39.1'
 
 class AboutDialog():
     __instance = None
