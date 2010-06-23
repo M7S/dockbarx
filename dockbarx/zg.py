e5db4f4343176ea6dbb6fa95683a13feb7a59aec
Matias Särs <msevens@kolumbus.fi>
2010-06-23 14:08:20 +0300

    - Copyright notion for zg.py updated.

--- dockbarx/zg.py
+++ dockbarx/zg.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+#   zg.py
+#	Copyright 2010 Siegfried-Angel Gevatter Pujals and Matias Sars
+#
+#
+#	DockbarX is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	DockbarX is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with dockbar.  If not, see <http://www.gnu.org/licenses/>.
 
 # This code is based on a python snipet.
 # SNIPPET_NAME: Recently used items
