0b07cfc67022314b7f57c293d5413223b8d3d8cd
Matias Särs <matias@matias-laptop>
2010-07-16 12:40:47 +0300

    - Fix for launching files with uris that contains ' or ".

--- dockbarx/common.py
+++ dockbarx/common.py
@@ -171,9 +171,16 @@ class DesktopEntry(xdg.DesktopEntry.DesktopEntry):
             uri_list = uri.split('\n')
             for uri in uri_list:
                 uri = uri.rstrip()
-                uris.append(uri)
-
+                
                 file = uri
+                
+                
+                # Nautilus and zeitgeist don't encode ' and " in uris and
+                # that's needed if we should launch with /bin/sh -c
+                uri = uri.replace("'", "%27")
+                uri = uri.replace('"', "%22")
+                uris.append(uri)
+                
                 if file.startswith('file://'):
                     file = file[7:]
                 file = file.replace("%20","\ ")
