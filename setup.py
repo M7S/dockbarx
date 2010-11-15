be159e79289814103ed0544ae5adfa822884cfb4
Matias Särs <msevens@kolumbus.fi>
2010-11-15 22:16:00 +0200

    - Changed the default theme. The new default theme is made by BigRZA. The old default theme is renamed 'Old'. - Added the sunny colors theme by BigRZA because it's the most beautiful theme made for dark panels out there (IMHO, of course). - Removed human_bar theme since it's pretty much an older version of the default theme.

--- setup.py
+++ setup.py
@@ -88,8 +88,9 @@ cmdclass = {
 
 data_files=[('/usr/share/dockbarx/themes', ['themes/default.tar.gz',
                                             'themes/Gaia.tar.gz',
-                                            'themes/human_bar.tar.gz',
+                                            'themes/old.tar.gz',
                                             'themes/minimalistic.tar.gz',
+                                            'themes/sunny-c.tar.gz',
                                             'themes/new_theme.tar.gz', ]),
             ('/usr/bin', ['dockbarx_factory.py', 'dbx_preference.py']),
             ('/usr/lib/bonobo/servers', ['GNOME_DockBarXApplet.server']),
