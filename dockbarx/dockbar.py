296775d31721a1deb8fc8c502e45634e84b66aa2
Matias Särs <m7s@bastu.net>
2014-06-19 20:03:57 +0300

    Brought up version number to 0.91.2

--- dockbarx/dockbar.py
+++ dockbarx/dockbar.py
@@ -37,7 +37,7 @@ from log import logger
 import i18n
 _ = i18n.language.gettext
 
-VERSION = "0.91.1"
+VERSION = "0.91.2"
 
 
 ATOM_WM_CLASS = gtk.gdk.atom_intern("WM_CLASS")
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ import msgfmt
 import os
 import sys
 
-VERSION = "0.91.1"
+VERSION = "0.91.2"
 
 class build_trans(cmd.Command):
     description = "Compile .po files into .mo files"
